132a65afd9abdddf18e3206b461e91c0e47c2488
Jim Yeh <lemonlatte@gmail.com>
2017-12-23 00:22:44 +0800

    Add facebook login for firebase

---
+++ ui/src/app/login.vue
@@ -0,0 +1,73 @@
+<style>
+  .login {
+    text-align: center;
+  }
+
+  .fb-login {
+    display: inline-block;
+    background-color: #31589c;
+    color: white;
+    text-align: center;
+    padding: 8px;
+    font-size: 16px;
+    width: 200px;
+    height: 40px;
+    cursor: pointer;
+  }
+</style>
+
+<template>
+  <div class="login">
+    <p>Sign in using:</p>
+    <div class="fb-login" @click="fbLogin">
+      <i class="fa fa-facebook-official"></i> Facebook</div>
+  </div>
+</template>
+
+<script>
+  const firebase = require("firebase/app");
+  require("firebase/auth");
+  const provider = new firebase.auth.FacebookAuthProvider();
+  export default {
+    methods: {
+      async fbLogin() {
+        try {
+          let u = await firebase.auth().signInWithRedirect(provider);
+          //   let u = await firebase.auth().signInWithPopup(provider);
+          let result = await firebase.auth().getRedirectResult()
+          if (result.credential) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+          }
+          // The signed-in user info.
+          var user = result.user;
+          console.log(user)
+          let redirect = this.$route.query.redirect || "/"
+          this.$router.push({
+            path: redirect
+          })
+        } catch (err) {
+          console.log(err)
+          // if (err.code === "auth/account-exists-with-different-credential") {
+          //   await firebase.auth().signInWithCredential(err.credential)
+          // }
+        }
+      },
+
+      async login() {
+        try {
+          let u = await firebase.auth().signInWithEmailAndPassword(this.username, this.password)
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    },
+
+    data() {
+      return {
+        username: "",
+        password: "",
+      }
+    }
+  }
+</script>
--- ui/src/main.js
+++ ui/src/main.js
@@ -2,15 +2,44 @@ var Vue = require('vue')
 var VueRouter = require('vue-router')
 Vue.use(VueRouter)
 
+var firebase = require("firebase/app");
+require("firebase/auth");
+
+var config = {
+  apiKey: "AIzaSyDKs8ugaxIPshjQNKGG3CvxwDuSQQRqMSk",
+  authDomain: "happy-ledger.firebaseapp.com",
+  databaseURL: "https://happy-ledger.firebaseio.com",
+  projectId: "happy-ledger",
+  storageBucket: "happy-ledger.appspot.com",
+  messagingSenderId: "644049233724"
+};
+firebase.initializeApp(config);
+
 var Main = require('./app/main.vue')
 var History = require('./app/history.vue')
+var Login = require('./app/login.vue')
 
 var routes = [
+  {path: '/login', component: Login},
   {path: '/', component: History},
   {path: '*', component: History}
 ]
 
 var router = new VueRouter({routes})
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log('logged in as user:', user.uid)
+    localStorage.setItem("uid", user.uid)
+    if (app.$route.path === "/login") {
+      app.$router.push({path: '/'})
+    }
+  } else {
+    console.log('not logged in')
+    localStorage.removeItem("uid")
+    app.$router.push({path: '/login', redirect: app.$route.path})
+  }
+})
 
 var app = new Vue({
   router,
